e1657dfd7b260463dea75bb78202dfee64446eda
Aman Shahid Pathan <87351832+MrGabby@users.noreply.github.com>
2023-05-02 02:00:38 +0530

    Add project files.

---
+++ Crop_Deal/Controllers/User.cs
@@ -0,0 +1,95 @@
+﻿
+using Crop_Deal.Data;
+using Crop_Deal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Crop_Deal.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UserController : Controller
+    {
+        private readonly ApiDbContext context;
+
+        public UserController(ApiDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostBrand(User b)
+        {
+            context.Users.Add(b);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBrand), new { id = b.User_id }, User);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetBrand(int User_id)
+        {
+            if (context.Users == null)
+            {
+                return NotFound();
+            }
+            var b = await context.Users.FindAsync(User_id);
+            if (b == null)
+            {
+                return NotFound();
+            }
+            return b;
+        }
+
+
+        /*     
+         *     
+         *     {
+        "user_id": 0,
+        "user_name": "aman",
+        "user_contact": "1234567890",
+        "user_roles": "admin",
+        "email_id": "aman@gmail.com",
+        "password": 0,
+        "dbo": "2023-05-01T20:17:07.400Z",
+        "address": "Pune",
+        "bank_name": "SBI",
+        "bank_account_no": "12345678",
+        "ifsc": "123",
+        "is_subscribe": true,
+        "is_Active": true
+      }
+         *     
+         *     [HttpGet("{id}")]
+              public async Task<IActionResult> GetUserById(int id)
+              {
+                  var user = await context.Users.FindAsync(id);
+
+                  if (user == null)
+                  {
+                      return NotFound();
+                  }
+
+                  return Ok(user);
+              }*/
+
+        /*   [HttpPost]
+           public async Task<ActionResult<User>> CreateUser(User u)
+           { 
+
+          *//*     var user = new User
+               {
+                   User_name = name,
+                   Email_id = email,
+                   Is_Active = is_active
+               };*//*
+
+               context.Users.Add(u);
+               await context.SaveChangesAsync();
+
+               return CreatedAtAction(nameof(GetUserById), new { id = u.User_id }, u);
+           }*/
+    }
+}
---
+++ Crop_Deal/Data/ApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Crop_Deal.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Crop_Deal.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext>options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Crop> Crops { get; set; }
+
+        public DbSet<Crop_detail> Crop_details { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+    }
+}
